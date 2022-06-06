42106317a0db0d7c1b992b671b8753bea1dd6cfb
Arttu <arttu@AM-C4R27ML6TL.local>
2022-06-06 19:31:01 +0300

    add share of stable rate, e-mode parameters

--- services/data.tsx
+++ services/data.tsx
@@ -3,6 +3,8 @@ import {
   UiPoolDataProvider,
   ChainId,
 } from '@aave/contract-helpers';
+import { formatReserves } from '@aave/math-utils';
+import dayjs from 'dayjs';
 
 const riskParameters: string[] = []
 
@@ -16,6 +18,8 @@ const fetchReservesAny = async (
     marketName: string
   }
   ) => {
+
+  const currentTimestamp = dayjs().unix();
 
 
   const lendingPoolAddressProvider = config.LENDING_POOL_ADDRESS_PROVIDER
@@ -36,10 +40,19 @@ const fetchReservesAny = async (
     const reserves = await poolDataProviderContract.getReservesHumanized({
       lendingPoolAddressProvider,
     });
+
+    const formattedPoolReserves = formatReserves({
+      reserves: reserves.reservesData,
+      currentTimestamp,
+      marketReferenceCurrencyDecimals:
+        reserves.baseCurrencyData.marketReferenceCurrencyDecimals,
+      marketReferencePriceInUsd: reserves.baseCurrencyData.marketReferenceCurrencyPriceInUsd,
+    });
    
-    const reservesArray = reserves.reservesData.map(n => 
+    const reservesArray = formattedPoolReserves.map(n => 
       ({
         symbol: n.symbol, 
+        usageAsCollateral: n.usageAsCollateralEnabled ? 'True' : 'False',
         baseLTVasCollateral: parseInt(n.baseLTVasCollateral)/100 + '%', 
         reserveLiquidationThreshold: parseInt(n.reserveLiquidationThreshold)/100 + '%',
         liqBonus: parseInt(n.reserveLiquidationBonus.slice(-3))/100 + '%',
@@ -50,11 +63,17 @@ const fetchReservesAny = async (
         stableBorrowRate:
         ((((1 + ((parseInt(n.stableBorrowRate)/(10**27)) / 31536000)) ** 31536000) - 1 ) * 100).toFixed(2) + '%'
         ,
-        avgStableBorrowRate: ((((1 + ((parseInt(n.averageStableRate)/(10**27)) / 31536000)) ** 31536000) - 1 ) * 100).toFixed(2) + '%' , 
-        optimalUsageRatio: (parseInt(n.optimalUsageRatio)/(10**27)).toFixed(2),
+        stableRateShare: parseInt(n.totalDebtUSD) === 0 || parseInt(n.totalStableDebtUSD) === 0  ? '0%' : ((parseInt(n.totalStableDebtUSD)/parseInt(n.totalDebtUSD)) * 100).toFixed(2) + '%' , 
+        optimalUsageRatio: ((parseInt(n.optimalUsageRatio)/(10**27)) * 100 ).toFixed(0) + '%',
         canBorrow: n.borrowingEnabled ? 'True' : 'False',
         stableBorrowingEnabled: n.stableBorrowRateEnabled ? 'True' : 'False',
-        assetLink: 'https://app.aave.com/reserve-overview/?underlyingAsset=' + n.id.slice(n.id.indexOf('-') + 1, n.id.lastIndexOf('-')) + '&marketName=' + config.marketName
+        assetLink: 'https://app.aave.com/reserve-overview/?underlyingAsset=' + n.id.slice(n.id.indexOf('-') + 1, n.id.lastIndexOf('-')) + '&marketName=' + config.marketName,
+        debtCeiling: (parseInt(n.debtCeiling) / 10**8).toFixed(2) + 'M',
+        supplyCap: (parseInt(n.supplyCap) / 10**9).toFixed(2) + 'B',
+        borrowCap: (parseInt(n.borrowCap) / 10**9).toFixed(2) + 'B',
+        eModeLiquidationBonus: parseInt((n.eModeLiquidationBonus).toString().slice(-3))/100 + '%',
+        eModeLiquidationThereshold: n.eModeLiquidationThreshold/100 + '%',
+        eModeLtv: n.eModeLtv/100 + '%'
       }));
     
     return reservesArray
