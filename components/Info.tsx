dd2cc7d5b281abe9f64de34f15fb8c0de81bdf9e
Arttu <arttu@AM-C4R27ML6TL.local>
2022-07-07 14:11:18 +0300

    cta fix

--- components/Info.tsx
+++ components/Info.tsx
@@ -3,7 +3,7 @@ import { Typography } from "@mui/material"
 const Info = (props: { marketSelected: boolean; missingProtocol: boolean }) => {
     return(
       <Typography align='center' variant="h6" sx={{ display:"flex" , alignItems:"center", justifyContent:"center" , p: 8}}>
-        {!props.marketSelected ? 'please select protocol and market for table to populate' : ''}
+        {!props.marketSelected && !props.missingProtocol ? 'please select protocol and market for table to populate' : ''}
         {props.missingProtocol ? 'want to see config for another protocol? contribute to the github'  : ''} 
       </Typography>        
     )
