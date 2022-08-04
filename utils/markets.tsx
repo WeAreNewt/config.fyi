f64b4074492a61bacd85aa17cdc5d4d6631b4067
JosepBove <josep@aave.com>
2022-08-04 14:16:25 +0100

    Add has link detection

--- utils/markets.tsx
+++ utils/markets.tsx
@@ -2,57 +2,57 @@
 import { marketConfig } from '../utils/marketconfig';
 
 export const markets = {
-v2 : [
-    {
-    name: 'ethereum',
-    config: marketConfig.ethereum
-    },
-    {
-    name: 'eth amm',
-    config: marketConfig.ethamm
-    },
-    {
-    name: 'avalanche',
-    config: marketConfig.avalanche
-    },
-    {
-    name: 'polygon',
-    config: marketConfig.polygon
-    }
-],
-v3 : [
-    {
-    name: 'arbitrum',
-    config: marketConfig.arbitrum
-    },
-    {
-    name: 'avalanche',
-    config: marketConfig.avalanchev3
-    },
-    {
-    name: 'fantom',
-    config: marketConfig.fantom
-    },
-    {
-    name: 'harmony',
-    config: marketConfig.harmony
-    },
-    {
-    name: 'optimism',
-    config: marketConfig.optimism
-    },
-    {
-    name: 'polygon',
-    config: marketConfig.polygonv3
-    }
-],
-univ3 : [{
-    name: 'all'
-}],
-crvv2 : [{
-    name: 'all'
-}],
-test : [{
-    name: 'test'
-}]
+    v2 : [
+        {
+        name: 'ethereum',
+        config: marketConfig.ethereum
+        },
+        {
+        name: 'eth amm',
+        config: marketConfig.ethamm
+        },
+        {
+        name: 'avalanche',
+        config: marketConfig.avalanche
+        },
+        {
+        name: 'polygon',
+        config: marketConfig.polygon
+        }
+    ],
+    v3 : [
+        {
+        name: 'arbitrum',
+        config: marketConfig.arbitrum
+        },
+        {
+        name: 'avalanche',
+        config: marketConfig.avalanchev3
+        },
+        {
+        name: 'fantom',
+        config: marketConfig.fantom
+        },
+        {
+        name: 'harmony',
+        config: marketConfig.harmony
+        },
+        {
+        name: 'optimism',
+        config: marketConfig.optimism
+        },
+        {
+        name: 'polygon',
+        config: marketConfig.polygonv3
+        }
+    ],
+    univ3 : [{
+        name: 'all'
+    }],
+    crvv2 : [{
+        name: 'all'
+    }],
+    test : [{
+        name: 'test'
+    }]
 }
