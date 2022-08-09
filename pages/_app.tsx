be998594a5be81802bea70229b134c0f6de1137c
JosepBove <josep@aave.com>
2022-08-09 15:16:09 +0100

    Add datadog

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import { datadogRum } from '@datadog/browser-rum';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  datadogRum.init({
+    applicationId: process.env.DATADOG_APPLICATION_ID!,
+    clientToken: process.env.DATADOG_CLIENT_TOKEN!,
+    site: 'datadoghq.eu',
+    service:'newt',
+    env:'staging',
+    sampleRate: 100,
+    premiumSampleRate: 100,
+    trackInteractions: true,
+    defaultPrivacyLevel:'mask-user-input'
+  });
+
+  datadogRum.startSessionReplayRecording();
   return( <Component {...pageProps} />)
 }
 
