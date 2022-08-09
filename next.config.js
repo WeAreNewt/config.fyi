b72c3b2fccf8821144a8e76aeb6bb720ef8b8820
JosepBove <josep@aave.com>
2022-08-09 18:04:35 +0100

    Bind env variables

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    DATADOG_APPLICATION_ID: process.env.DATADOG_APPLICATION_ID,
+    DATADOG_CLIENT_TOKEN: process.env.DATADOG_CLIENT_TOKEN
+  }
 }
 
 module.exports = nextConfig
