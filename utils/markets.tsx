d25f1e44285e3e945ac16efd1e2f2dcd644c525d
Mark Hinschberger <foodaka@users.noreply.github.com>
2023-03-06 12:10:49 +0000

    feat: v3 eth

--- utils/markets.tsx
+++ utils/markets.tsx
@@ -1,60 +1,67 @@
-
-import { marketConfig } from '../utils/marketconfig';
+import { marketConfig } from "../utils/marketconfig";
 
 export const markets = {
-    v2 : [
-        {
-        name: 'ethereum',
-        config: marketConfig.ethereum
-        },
-        {
-        name: 'eth amm',
-        config: marketConfig.ethamm
-        },
-        {
-        name: 'avalanche',
-        config: marketConfig.avalanche
-        },
-        {
-        name: 'polygon',
-        config: marketConfig.polygon
-        }
-    ],
-    v3 : [
-        {
-        name: 'arbitrum',
-        config: marketConfig.arbitrum
-        },
-        {
-        name: 'avalanche',
-        config: marketConfig.avalanchev3
-        },
-        {
-        name: 'fantom',
-        config: marketConfig.fantom
-        },
-        {
-        name: 'harmony',
-        config: marketConfig.harmony
-        },
-        {
-        name: 'optimism',
-        config: marketConfig.optimism
-        },
-        {
-        name: 'polygon',
-        config: marketConfig.polygonv3
-        }
-    ],
-    benqi : [
-        {
-            name: 'avalanche'
-        }
-    ],
-    univ3 : [{
-        name: 'all'
-    }],
-    crvv2 : [{
-        name: 'all'
-    }] 
-}
+  v2: [
+    {
+      name: "ethereum",
+      config: marketConfig.ethereum,
+    },
+    {
+      name: "eth amm",
+      config: marketConfig.ethamm,
+    },
+    {
+      name: "avalanche",
+      config: marketConfig.avalanche,
+    },
+    {
+      name: "polygon",
+      config: marketConfig.polygon,
+    },
+  ],
+  v3: [
+    {
+      name: "ethereum",
+      config: marketConfig.ethereumv3,
+    },
+    {
+      name: "arbitrum",
+      config: marketConfig.arbitrum,
+    },
+    {
+      name: "avalanche",
+      config: marketConfig.avalanchev3,
+    },
+    {
+      name: "fantom",
+      config: marketConfig.fantom,
+    },
+    {
+      name: "harmony",
+      config: marketConfig.harmony,
+    },
+    {
+      name: "optimism",
+      config: marketConfig.optimism,
+    },
+    {
+      name: "polygon",
+      config: marketConfig.polygonv3,
+    },
+  ],
+  benqi: [
+    {
+      name: "avalanche",
+    },
+  ],
+  univ3: [
+    {
+      name: "all",
+    },
+  ],
+  crvv2: [
+    {
+      name: "all",
+    },
+  ],
+};
