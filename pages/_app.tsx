c45b4656e18529d77a1435cc9aef41820f58db52
Mark Hinschberger <foodaka@users.noreply.github.com>
2023-03-06 08:53:12 -0500

    Revert "feat: v3 eth"

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,35 +1,9 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-import * as Fathom from "fathom-client";
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  useEffect(() => {
-    // Initialize Fathom when the app loads
-    // Example: yourdomain.com
-    //  - Do not include https://
-    //  - This must be an exact match of your domain.
-    //  - If you're using www. for your domain, make sure you include that here.
-    Fathom.load(process.env.NEXT_PUBLIC_FATHOM_ID ?? "", {
-      includedDomains: ["config.fyi", "www.config.fyi"],
-    });
-
-    function onRouteChangeComplete() {
-      Fathom.trackPageview();
-    }
-    // Record a pageview when route changes
-    router.events.on("routeChangeComplete", onRouteChangeComplete);
-
-    // Unassign event listener
-    return () => {
-      router.events.off("routeChangeComplete", onRouteChangeComplete);
-    };
-  }, []);
-
-  return <Component {...pageProps} />;
+  return( <Component {...pageProps} />)
 }
 
-export default MyApp;
+export default MyApp
