ea8887b88fe4fad5edc57e19f2243fea9a66e043
defispartan <schmi651@msu.edu>
2023-07-25 02:53:31 -0500

    feat: add borrowableInIsolationMode column to Aave V3 reserves (#44)
    * feat: add borrwableInIsolationMode column to Aave V3 reserves
    
    * chore: bump address library
    
    * chore: bump utils packages
    
    * chore: public mainnet and polygon rpc

--- utils/headers.tsx
+++ utils/headers.tsx
@@ -1,7 +1,7 @@
 
 export const headers = {
     v2: ['asset', 'can collateral', 'LTV', 'liquidation thereshold', 'liquidation bonus', 'reserve factor', 'can borrow?', 'optimal utilization', 'variable borrow rate', 'can borrow stable?', 'stable borrow rate', 'share of stable rate', ''],
-    v3: ['asset', 'can collateral', 'LTV', 'liquidation thereshold', 'liquidation bonus', 'reserve factor', 'can borrow?', 'optimal utilization', 'variable borrow rate', 'can borrow stable?', 'stable borrow rate', 'share of stable rate', 'debt ceiling', 'supply cap', 'borrow cap', 'emode LTV', 'emode liq thereshold', 'emode liq bonus', ''],
+    v3: ['asset', 'can collateral', 'LTV', 'liquidation thereshold', 'liquidation bonus', 'reserve factor', 'can borrow?', 'optimal utilization', 'variable borrow rate', 'can borrow stable?', 'stable borrow rate', 'share of stable rate', 'debt ceiling', 'supply cap', 'borrow cap', 'emode LTV', 'emode liq thereshold', 'emode liq bonus', 'borrowable in iso mode', ''],
     benqi: ['asset', 'collateral factor', 'reserve factor', 'close factor', 'liquidation incentive'],
     univ3: [],
     crvv2: []
