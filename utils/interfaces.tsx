ab229463def8a900c2e347c8aa4631f2ad612cd2
defispartan <andrew@aave.com>
2023-11-07 21:47:44 -0600

    feat: frozen and paused flags

--- utils/interfaces.tsx
+++ utils/interfaces.tsx
@@ -1,54 +1,54 @@
 interface Aavev2 {
-    symbol: string,
-    canCollateral: string,
-    LTV: string,
-    liqThereshold: string,
-    liqBonus: string,
-    reserveFactor: string,
-    canBorrow: string,
-    optimalUtilization: string,
-    varBorrowRate: string,
-    canBorrowStable: string,
-    stableBorrowRate: string,
-    shareOfStableRate: string,
-    assetLink: string,
+  symbol: string;
+  frozen: string;
+  paused: string;
+  canCollateral: string;
+  LTV: string;
+  liqThereshold: string;
+  liqBonus: string;
+  reserveFactor: string;
+  canBorrow: string;
+  optimalUtilization: string;
+  varBorrowRate: string;
+  canBorrowStable: string;
+  stableBorrowRate: string;
+  shareOfStableRate: string;
+  assetLink: string;
 }
 
 interface Aavev3 {
-    symbol: string,
-    canCollateral: string,
-    LTV: string,
-    liqThereshold: string,
-    liqBonus: string,
-    reserveFactor: string,
-    canBorrow: string,
-    optimalUtilization: string,
-    varBorrowRate: string,
-    canBorrowStable: string,
-    stableBorrowRate: string,
-    shareOfStableRate: string,
-    debtCeiling: string,
-    supplyCap: string,
-    borrowCap: string,
-    eModeLtv: string
-    eModeLiquidationThereshold: string,
-    eModeLiquidationBonus: string,
-    assetLink: string,
+  symbol: string;
+  frozen: string;
+  paused: string;
+  canCollateral: string;
+  LTV: string;
+  liqThereshold: string;
+  liqBonus: string;
+  reserveFactor: string;
+  canBorrow: string;
+  optimalUtilization: string;
+  varBorrowRate: string;
+  canBorrowStable: string;
+  stableBorrowRate: string;
+  shareOfStableRate: string;
+  debtCeiling: string;
+  supplyCap: string;
+  borrowCap: string;
+  eModeLtv: string;
+  eModeLiquidationThereshold: string;
+  eModeLiquidationBonus: string;
+  assetLink: string;
 }
 
 interface Benqi {
-    symbol: string,
-    collateralFactor: string,
-    reserveFactor: string,
-    closeFactor: string,
-    liquidationIncentive: string,
-    assetLink?: string
+  symbol: string;
+  collateralFactor: string;
+  reserveFactor: string;
+  closeFactor: string;
+  liquidationIncentive: string;
+  assetLink?: string;
 }
 
-export type assetType = (Aavev2 | Aavev3 | Benqi)
+export type assetType = Aavev2 | Aavev3 | Benqi;
 
-export type {
-    Aavev2,
-    Aavev3,
-    Benqi
-}
+export type { Aavev2, Aavev3, Benqi };
